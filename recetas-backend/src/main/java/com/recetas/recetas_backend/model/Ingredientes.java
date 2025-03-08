ab1bcee8116c6c0f0fe1dc2b6a2acdafd5aca2ae
YanferC <ycastro11@udi.edu.co>
2025-03-07 20:16:59 -0500

    actualización backend

--- recetas-backend/src/main/java/com/recetas/recetas_backend/model/Ingredientes.java
+++ recetas-backend/src/main/java/com/recetas/recetas_backend/model/Ingredientes.java
@@ -1,4 +1,16 @@
 package com.recetas.recetas_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "ingredientes")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Ingredientes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
 }
